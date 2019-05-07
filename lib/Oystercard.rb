2d254e24882cbc03155b84f8497c19eb44a7bdf5
Vijay Kurian <kurianvijay7@gmail.com>
2019-05-07 14:59:57 +0100

    Created default value method balance => :balance

--- lib/Oystercard.rb
+++ lib/Oystercard.rb
@@ -1,1 +1,14 @@
+class Oystercard
+  DEFAULT_VALUE = 0
 
+  attr_reader :balance
+
+  def initialize(value = DEFAULT_VALUE)
+    @balance = value
+  end
+
+  # def balance
+  #   @value
+  # end
+
+end
--- spec/Oystercard_spec.rb
+++ spec/Oystercard_spec.rb
@@ -1,3 +1,10 @@
+require 'Oystercard'
 describe Oystercard do
+
+ describe "balance" do
+   it "Has a default balance of 0" do
+     expect(subject.balance).to eq(0)
+   end
+ end
 
 end
