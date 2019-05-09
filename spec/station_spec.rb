d516d74aa9e9ea0eea748815de3aaf5e4f1923c7
Sam <samkitchen94@gmail.com>
2019-05-09 16:19:15 +0100

    add station class

--- spec/station_spec.rb
+++ spec/station_spec.rb
@@ -1,4 +1,13 @@
 require 'station'
-describe Station do 
+describe Station do
 
+  it "stores station name when it initializes" do
+    station = Station.new("here", 4)
+    expect(station.name).to eq("here")
+  end
+
+  it "stores the zone when it initializes" do
+    station = Station.new("here", 4)
+    expect(station.zone).to eq(4)
+  end
 end
